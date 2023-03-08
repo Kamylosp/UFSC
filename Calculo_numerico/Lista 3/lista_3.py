c4fc75089980eec99e078d43948297890bf28a44
Kamylosp <kamyloserafimporto@gmail.com>
2023-03-08 18:37:55 -0300

    lista 3

---
+++ Calculo_numerico/Lista 3/lista_3.py
@@ -0,0 +1,7 @@
+# Vai retornar uma lista reversa da recebida
+def reverter (L):
+    pass
+
+# Vai reverter a lista enviada
+def reversa (L):
+    L = [1, 2, 3]
--- Calculo_numerico/Lista 3/prints.py
+++ Calculo_numerico/Lista 3/prints.py
@@ -1,1 +1,5 @@
 import lista_3 as l
+
+L = [3, 2, 1]
+l.reversa(L)
+print("L:", L)
