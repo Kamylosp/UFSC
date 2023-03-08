8b1806af355c2e5d47579367272aeca68d0e34dd
Kamylosp <kamyloserafimporto@gmail.com>
2023-03-08 09:17:42 -0300

    Aula e lista 2

--- Calculo_numerico/Lista 2/lista_2.py
+++ Calculo_numerico/Lista 2/lista_2.py
@@ -1,30 +1,42 @@
 alfabeto_minusculo = "abcdefghijklmnopqrstuvwxyz"
 
-def Celcius_para_Farenheit(T):
-    return T*1.8 + 32
+def Celsius_para_Farenheit(T):
+    return (T*1.8 + 32)
 
 def minusculo(c):
     if c in alfabeto_minusculo:
         return True
     return False
 
+def muda_maiusculo(c):
+    if minusculo(c):
+        return chr(ord(c)-32)
+    return c
+
 def frase_maiusculo(fr):
-    string = ""
-    for c in fr:
-        if minusculo(c):
-            c = chr(ord(c)-32)
-        string += c
-    return string
+    if (len(fr) > 1):
+        return muda_maiusculo(fr[0]) + frase_maiusculo(fr[1:])
+    else:
+        return muda_maiusculo(fr[0])
 
-def Fib_gen_in(a,b,n):
-    pass
-
-
+def Fin_gen_int(a, b, n):
+    if (n == 0):
+        return a
+    elif (n == 1):
+        return b
+    else:
+        return Fin_gen_int(a, b, n-1) + Fin_gen_int(a, b, n-2)
 
 
 
 # Prints
-print(f"Temperatura em Farenheit: {Celcius_para_Farenheit(32)}")
+print(f"Temperatura em Farenheit: {Celsius_para_Farenheit(32)}")
 print(f"a é minúsculo? {minusculo('a')}")
 print(f"A é minúsculo? {minusculo('A')}")
 print(f"Transformando string em maiuscula: {frase_maiusculo('ola TUDO joia?')}")
+a, b = 3, 2
+print("Fibonacci:", Fin_gen_int(a, b, 0))
+print("Fibonacci:", Fin_gen_int(a, b, 1))
+print("Fibonacci:", Fin_gen_int(a, b, 2))
+print("Fibonacci:", Fin_gen_int(a, b, 3))
+print("Fibonacci:", Fin_gen_int(a, b, 4))
