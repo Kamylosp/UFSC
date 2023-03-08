d68bfbd72a93f89d37d6da2cb6b27f6f4e0a929a
Kamylosp <kamyloserafimporto@gmail.com>
2023-03-08 18:53:34 -0300

    Lista 3

--- Calculo_numerico/Lista 3/lista_3.py
+++ Calculo_numerico/Lista 3/lista_3.py
@@ -1,7 +1,10 @@
-# Vai retornar uma lista reversa da recebida
-def reverter (L):
-    pass
-
 # Vai reverter a lista enviada
+def reverter (L):
+    for i in range(0, (len(L)//2)):
+        L[i], L[len(L)-1] = L[len(L)-1], L[i]
+
+# Vai retornar uma lista reversa da recebida
 def reversa (L):
-    L = [1, 2, 3]
+    A = L[:]
+    reverter(A)
+    return A
--- Calculo_numerico/Lista 3/prints.py
+++ Calculo_numerico/Lista 3/prints.py
@@ -1,5 +1,23 @@
 import lista_3 as l
 
+# Questão 1
+
+
+# Questão 2
+
+
+# Questão 3
 L = [3, 2, 1]
-l.reversa(L)
-print("L:", L)
+A = l.reversa(L)
+print("L:          ", L)
+l.reverter(L)
+print("reverter(L):", L)
+print("reversa(L): ", A)
+
+
+# Questão 4
+
+
+
+
+# Questão 5
