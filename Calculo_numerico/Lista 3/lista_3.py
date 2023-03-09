4b5cc1677c7e6c5906314e33e0dd4c2dd0419506
Kamylosp <kamyloserafimporto@gmail.com>
2023-03-09 11:06:31 -0300

    Lista 3

--- Calculo_numerico/Lista 3/lista_3.py
+++ Calculo_numerico/Lista 3/lista_3.py
@@ -1,3 +1,33 @@
+# Questão 1
+
+n = 11
+base = 9.3132257e-10
+
+def fatorial (x):
+    if (x <= 1):  
+        return 1
+    else:       
+        return x*fatorial(x-1)
+
+def exp(base, expoente):
+    output = 1
+    for i in range (0, expoente):
+        output *= base
+    return output
+
+def sin (x):
+    soma = 0
+    for i in range (n):
+        soma += (1 - 2*(i%2))/(fatorial(2*i+1)) * (exp(x, 2*i+1))
+    return soma
+
+def cos (x):
+    soma = 0
+    for i in range (n):
+        soma += (1 - 2*(i%2))/(fatorial(2*i)) * (exp(x, 2*i))
+    return soma
+        
+
 # Vai reverter a lista enviada
 def reverter (L):
     for i in range(0, (len(L)//2)):
