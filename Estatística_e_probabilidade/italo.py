365c40dcb5c2f0043848a4dd2c1fed4a7c86561d
Kamylosp <kamyloserafimporto@gmail.com>
2023-03-14 18:52:21 -0300

    grafico

--- Estatística_e_probabilidade/italo.py
+++ Estatística_e_probabilidade/italo.py
@@ -1,12 +1,17 @@
 from graficos import boxplot as bx
+import operacoes as op
 import numpy as np
 
-np.random.seed(10)
 #lista = np.random.normal((3, 5, 4, 5, 6), (1.25, 1.00, 1.25, 1.8, 2.25), (3, 5))
-#lista = np.random.normal((3, 5, 4), (1.25, 1.00, 1.25), (3, 3))
 
 lista = [[1.5, 2, 5, 9, 5], [3,4, 7, 1, 6], [5, 6, 2, 5, 4]]
 
+lista2 = []
+for i in range(5):
+    lista2.append(np.random.randint(8, size=10))
 
-grafico = bx(lista)
+op.printa_informacoes(lista[0])
+
+
+grafico = bx(lista2)
 grafico.mostrar_grafico()
