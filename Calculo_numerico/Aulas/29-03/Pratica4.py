a45cd91435112106c49297c2e799250d948ee8e2
Kamylosp <kamyloserafimporto@gmail.com>
2023-03-29 09:10:19 -0300

    pratica

--- Calculo_numerico/Aulas/29-03/Pratica4.py
+++ Calculo_numerico/Aulas/29-03/Pratica4.py
@@ -6,11 +6,3 @@ def medianas_linhas(A):
         # A[i].sort()
         x[i] = np.median(A[i])
     return x
-
-L=[[-10.6486474 , -14.5256196 , -12.22596635 , -7.74696165] ,
-[ 10.68030044 , 5.54195539 , 12.3896066 , 11.69923365]]
-
-print(medianas_linhas(L))
-
-
-L = []
