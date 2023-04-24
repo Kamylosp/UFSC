764c07e7d9ac829b0608f7c7bdafeaba11627fa5
Kamylosp <kamyloserafimporto@gmail.com>
2023-04-24 08:44:07 -0300

    lista 5

--- Calculo_numerico/Lista 5/lista_5.py
+++ Calculo_numerico/Lista 5/lista_5.py
@@ -38,22 +38,27 @@ def lista_5_ex_3 (c, v, t):
 
     return M
 
-print(lista_5_ex_3(1, 100, 50))
+# print(lista_5_ex_3(1, 100, 50))
+
+
 
 # Questão 4
+R = 0.518
+T = 273.15 - 40
+p = 65000
 
 def Q4_f(v, a, b):
-    return (120.7717)/(v - b) - a/(v*(v+b)*np.sqrt(233.15)) - 65
+    return (R*T)/(v - b) - a/(v*(v+b)*np.sqrt(T)) - p
 
 def Q4_f_(v, a, b):
-    return -120.772/np.power(v - b, 2) - (a/np.sqrt(233.15))* (b+2*v)/((v*v) * (b+v)**2)
+    return -(R*T)/np.power(v - b, 2) - (a/np.sqrt(T)) * (b+2*v)/((v*v) * (b+v)**2)
 
 
 def Q4_a(Tc, Pc):
-    return 0.114574348 * np.power(Tc, 2.5) / Pc
+    return 0.727*(R**2) * np.power(Tc, 2.5) / Pc
 
 def Q4_b(Tc, Pc):
-    return 0.0448588 * Tc / Pc
+    return 0.0866 * R * Tc / Pc
 
 def lista_5_ex_4 (temp_crit, pres_crit):
     a = Q4_a(temp_crit, pres_crit)
@@ -62,25 +67,58 @@ def lista_5_ex_4 (temp_crit, pres_crit):
     print("a: ", a)
     print("b: ", b)
 
-    v, v_anterior = 0.5, 0
+    v, v_anterior = 0.006, 0
 
-    print("f(): ", Q4_f(v, a, b))
-    print("f'(): ", Q4_f_(v, a, b))
+    # print("f(): ", Q4_f(v, a, b))
+    # print("f'(): ", Q4_f_(v, a, b))
 
     while abs((v-v_anterior)/v) > tol:
-        print("v: ", v)
+        # print("v: ", v)
         v_anterior = v
         v = v - Q4_f(v, a, b)/Q4_f_(v, a, b)
 
     return(3/v)
 
-# temp_cri = 500
-# p_cri = 10000
+temp_cri = 500
+p_cri = 10000
 
-# # print(Q4_f(v, Q4_a(temp_cri, p_cri), Q4_b(temp_cri, p_cri)) + 65000)
+# print(Q4_f(v, Q4_a(temp_cri, p_cri), Q4_b(temp_cri, p_cri)) + 65000)
 # print(lista_5_ex_4(temp_cri, p_cri))
 
 # def teste(v, a, b):
 #     return (120.7717)/(v - b) - a/(v*(v+b)*np.sqrt(233.15))
 
 # print(teste(0.00253683, 64.04900765142928, 0.0022429399999999997))
+
+
+# Questão 5
+def Q5_f(q, j, n, p):
+    return (1- 1/np.power(1+j, n))*p / j - q
+
+def Q5_f_(q, j, n, p):
+    return ((-1 + 1/np.power(1 + j, n) + j * 1/np.power(1 + j, 1+n) * n)* p) / (j**2)
+
+def calculadora_do_cidadao (valor = None, juros = None, tempo = None, prestacao = None):
+    if not valor:
+        return (1 - np.power(1+juros, - tempo)) * (prestacao) / (juros)
+    
+    elif not juros:
+        j, j_anterior = 0.001, 0
+
+        # print(Q5_f(valor, j, tempo, prestacao), Q5_f_(valor, j, tempo, prestacao))
+
+        while abs((j-j_anterior)/j) > tol:
+            j_anterior = j
+            j = j - Q5_f(valor, j, tempo, prestacao)/Q5_f_(valor, j, tempo, prestacao)
+
+        return j
+
+    elif not tempo:
+        return abs(np.log(1- valor*juros/prestacao) / np.log(1+juros))
+
+    elif not prestacao:
+        return valor / ( (1-1/np.power(1+juros, tempo)) / juros)
+
+
+
+# Questão 6
--- Calculo_numerico/Lista 5/prints.py
+++ Calculo_numerico/Lista 5/prints.py
@@ -8,6 +8,22 @@ def f(h):
 def f_(h):
     return 10 * np.sqrt((4-h)*h)
 
-print(newton(f, f_, 0.5))
+# print(newton(f, f_, 0.5))
 
-print(np.power(2, 3))
+# print(np.power(2, 3))
+
+
+
+
+
+# Questão 5
+# print( calculadora_do_cidadao (juros =0.021 , tempo =12 , prestacao =2854.23))
+
+# print(calculadora_do_cidadao ( valor =30000 , tempo =12 , prestacao =2854.23))
+# print("0.02099970")
+
+# print(calculadora_do_cidadao ( valor =30000 , juros =0.021 , prestacao =2854.23))
+# print("12.0")
+
+# print(calculadora_do_cidadao ( valor =30000 , juros =0.021 , tempo =12))
+# print("2854.23")
